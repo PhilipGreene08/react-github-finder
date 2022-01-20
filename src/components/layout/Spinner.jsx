77bcad1ec6b8a40161e02a0726aa15ee3944e102
Phil Greene <philgreene08@gmail.com>
2022-01-19 21:10:03 -0600

    day 3

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,10 @@
+import spinner2 from './assets/spinner2.gif';
+function Spinner() {
+  return (
+    <div className='w-100 mt-20'>
+      <img width={180} className='text-center mx-auto' src={spinner2} alt='' />
+    </div>
+  );
+}
+
+export default Spinner;
