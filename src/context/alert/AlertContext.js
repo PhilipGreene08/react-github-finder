ef57913a73f31809acfceb5124d801f6380e293e
Phil Greene <philgreene08@gmail.com>
2022-01-25 21:12:10 -0600

    context api and dispatch practice

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -6,7 +6,25 @@ const AlertContext = createContext();
 export const AlertProvider = ({ children }) => {
   const initialState = null;
 
-  return <AlertContext.Provider>{children}</AlertContext.Provider>;
+  const [state, dispatch] = useReducer(alertReducer, initialState);
+
+  //set an alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: 'SET_ALERT',
+      payload: { msg, type },
+    });
+
+    setTimeout(() => {
+      dispatch({ type: 'REMOVE_ALERT' });
+    }, 3000);
+  };
+
+  return (
+    <AlertContext.Provider value={{ alert: state, setAlert }}>
+      {children}
+    </AlertContext.Provider>
+  );
 };
 
 export default AlertContext;
